bdec0e6f2d2d3a701baafc6b9643315022b8e678
sarath <sarathchandra0007@gmail.com>
2018-09-23 22:55:29 +0530

    Profile Editor

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -10,19 +10,24 @@ class LoginForm(FlaskForm):
     submit      = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired()])
-    email = StringField('Email',validators=[DataRequired(), Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    password2 = PasswordField('Re-enter Password', validators=[DataRequired(), EqualTo('password')])
+    username    = StringField('Username',validators=[DataRequired()])
+    email       = StringField('Email',validators=[DataRequired(), Email()])
+    password    = PasswordField('Password',validators=[DataRequired()])
+    password2   = PasswordField('Re-enter Password', validators=[DataRequired(), EqualTo('password')])
     submit      = SubmitField('Sign In')
 
     # custom validators
 
     def validate_username(self,username):
-        user = User.query.filter_by(username = username.data).first()
+        user    = User.query.filter_by(username = username.data).first()
         if user is not None:
             raise ValidationError('Username entered exists, enter another name')
     def validate_email(self,email):
-        email = User.query.filter_by(email = email.data).first()
+        email   = User.query.filter_by(email = email.data).first()
         if email is not None:
             raise ValidationError('Email entered exists, enter another email')
+
+class EditProfileForm(FlaskForm):
+    username    = StringField('Username',validators=[DataRequired()])
+    about_me    = TextAreaField('About me',validators=[Length(min=0,max=140)])
+    submit      = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template,flash,redirect, url_for,request
 from werkzeug.urls import url_parse
 from flask_login import login_user, current_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from datetime import datetime
 
@@ -86,3 +86,19 @@ def user(username):
     ]
 
     return render_template('user.html',posts=posts,user=user)
+
+@app.route('/editprofile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form=EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes were updated')
+        return redirect(url_for('index'))
+    if request.method=='GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template('edit_profile.html',form=form,title='Edit Profile')
