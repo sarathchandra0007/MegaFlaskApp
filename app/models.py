a214582fc90375690ad4716b9ace89fa1013d66d
sarath <sarathchandra0007@gmail.com>
2018-09-25 11:32:23 +0530

    Added users followers functionality and custom testcases

--- app/models.py
+++ app/models.py
@@ -4,6 +4,13 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+# auxilary table (many-many relation)
+followers = db.Table(
+    'followers',
+    db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id              = db.Column(db.Integer, primary_key=True)
     username        = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +19,11 @@ class User(UserMixin, db.Model):
     posts           = db.relationship('Post',backref='author',lazy='dynamic')
     about_me        = db.Column(db.String(140))
     last_seen       = db.Column(db.DateTime, default=datetime.utcnow())
+    followed        = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 
     def __repr__(self):
@@ -22,6 +34,25 @@ class User(UserMixin, db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+    def follow(self,user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self,user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)     #followed user posts
+        own = Post.query.filter_by(user_id=self.id)     #self posts
+        return followed.union(own).order_by(Post.timestamp.desc())  #Timestamp latest post first
 
     def avatar(self, size):
         md5=hashlib.md5
