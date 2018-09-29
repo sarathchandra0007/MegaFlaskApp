bd3c22427e75a175359b42c7fde40e35a1621e47
sarath <sarathchandra0007@gmail.com>
2018-09-29 19:41:58 +0530

    Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 #app.config['SECRET_KEY']='lucky'
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 login.login_view='login'       #when login_required recorator is mentioned in routes login route is triggered
 
 
